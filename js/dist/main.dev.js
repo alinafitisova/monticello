40b51bff970aba198d00f05e75effe2121f07df5
Alina_Fitisova <alinafitisova@gmail.com>
2020-12-28 01:05:29 +0200

    add liner-gradient for the header-slides

--- js/dist/main.dev.js
+++ js/dist/main.dev.js
@@ -6,4 +6,14 @@ $(document).ready(function () {
     $('.header__burger, .header__menu').toggleClass('active');
     $('body').toggleClass('lock');
   });
+  $('.header-slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 800,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 3000
+  });
 });
--- js/main.js
+++ js/main.js
@@ -3,5 +3,15 @@
         $('.header__burger, .header__menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
-    
+    $('.header-slider').slick({
+        dots: true,
+        arrows : false,
+        infinite: true,
+        speed: 800,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 3000,
+        });
 });
+
