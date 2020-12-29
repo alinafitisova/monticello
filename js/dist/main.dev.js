26bba0364bf8f711e3190f072c5b8d531ff512d6
Alina_Fitisova <alinafitisova@gmail.com>
2020-12-29 02:14:57 +0200

    creating the second slider

--- js/dist/main.dev.js
+++ js/dist/main.dev.js
@@ -16,4 +16,28 @@ $(document).ready(function () {
     autoplay: true,
     autoplaySpeed: 3000
   });
+  $('.slider-projects').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true,
+    responsive: [{
+      breakpoint: 1450,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2
+      }
+    }, {
+      breakpoint: 998,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }, {
+      breakpoint: 680,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }]
+  });
 });
--- js/main.js
+++ js/main.js
@@ -14,5 +14,34 @@
         autoplay: true,
         autoplaySpeed: 3000,
         });
+        $('.slider-projects').slick({
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            dots: true,
+            responsive: [
+              {
+                breakpoint: 1450,
+                settings: {
+                  slidesToShow:3,
+                  slidesToScroll:2,
+                }
+              },
+              {
+                breakpoint: 998,
+                settings: {
+                  slidesToShow:2,
+                  slidesToScroll:1,
+                }
+              },
+              {
+                breakpoint: 680,
+                settings: {
+                  slidesToShow:1,
+                  slidesToScroll:1,
+                }
+              }
+              
+            ]
+          });
 });
 
